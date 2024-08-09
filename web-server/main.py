6e7763b99024e3004ac4ca7766386e6983ca65eb
Jeison Arias <jeisonarias68@gmail.com>
2024-08-09 15:39:42 -0500

    Creamos un servidor web en web-server

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,25 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3, 4, 5]
+
+# @app.get('/contact')
+# def get_list():
+#     return{'name' : 'Jeison.site'}
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+    <h1> Pagina en HTML </h1>
+    <p> Este es el parrafo de contenido </p>
+
+    """
+
 
 def run():
     store.get_categories()
